2e70901a2515b8bcc11100739e046bbeaf17d64b
elmaccho <106253529+elmaccho@users.noreply.github.com>
2025-05-16 13:24:50 +0200

    Panel component init

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,14 @@
-<h1>test</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Panel administracyjny</title>
+</head>
+<body>
+    <x-panel>
+        tekst
+    </x-panel>
+</body>
+</html>
