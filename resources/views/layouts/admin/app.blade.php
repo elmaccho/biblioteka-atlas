5ed8d3248b3408281d5138fa2247c91dc116f7dc
elmaccho <106253529+elmaccho@users.noreply.github.com>
2025-05-19 14:36:37 +0200

    Naprawianie jsa

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -15,17 +15,56 @@
     <script src="https://kit.fontawesome.com/4798a03daf.js" crossorigin="anonymous"></script>
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/side-menu.css', 'resources/css/upper-menu.css', 'resources/js/side-menu.js', 'resources/css/panel/panel.css'])
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/css/side-menu.css',
+        'resources/css/upper-menu.css',
+        'resources/js/side-menu.js',
+        'resources/css/panel/panel.css'
+    ])
 </head>
 
 <body class="overflow-hidden">
     <div class="d-flex w-100" style="height: 100vh;">
         <div class="side-menu-container">
             <x-panel title="Panel administracyjny">
-                <a class="link-button" href="{{ route('home') }}">
+                {{-- <a class="link-button" href="{{ route('home') }}">
                     <i class="fa-solid fa-house"></i>
                     <span>Strona główna</span>
-                </a>
+                </a> --}}
+                {{-- <div class="mb-3">
+                    <button class="btn btn-primary w-100 text-start d-flex align-items-center justify-content-between"
+                        type="button" data-bs-toggle="collapse" data-bs-target="#category1" aria-expanded="false"
+                        aria-controls="category1">
+                        <div><i class="fa-solid fa-house me-2"></i>Strona główna</div>
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </button>
+                    <div class="collapse" id="category1">
+                        <div class="card card-body">
+                            <a href="#" class="btn btn-link text-start">Podkategoria 1</a>
+                            <a href="#" class="btn btn-link text-start">Podkategoria 2</a>
+                            <a href="#" class="btn btn-link text-start">Podkategoria 3</a>
+                        </div>
+                    </div>
+                </div> --}}
+
+                <div class="accordion" id="accordionPanelsStayOpenExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                                aria-controls="panelsStayOpen-collapseOne">
+                                Accordion Item #1
+                            </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                            <div class="accordion-body">
+                                <code>XDDDDDDDDDDd</code>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </x-panel>
         </div>
         <div class="content-container d-flex flex-column flex-grow-1">
