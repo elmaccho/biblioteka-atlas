d8f51cdd0631aee53385e867dfa839b87e7fbe9b
root <maciek.chojnacki22@wp.pl>
2025-06-17 19:56:32 +0200

    Konteneryzacja udana XD

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+	host: '0.0.0.0',
+	strictPort: true,
+	port: 5173,
+	cors: true,
+	},
+
     plugins: [
         laravel({
             input: [
