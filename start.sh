9ddcb8a4c9869589b880c872ff2f8a3595fc6bf8
root <root@ip-172-31-24-229.eu-west-1.compute.internal>
2025-06-18 17:14:56 +0000

    merge

--- start.sh
+++ start.sh
@@ -7,9 +7,18 @@ done
 
 
 echo "Instalowanie zaleznosci composera..."
-composer install --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev --no-scripts --no-progress
 
 echo "Wykonywanie migracji"
 php artisan migrate
+
+echo "Nadawanie uprawnien"
+chown -R www-data:www-data storage
+chown -R www-data:www-data bootstrap/cache
+
+chmod -R 775 storage
+chmod -R 775 bootstrap/cache
+
+php artisan cache:clear
 
 apache2-foreground
